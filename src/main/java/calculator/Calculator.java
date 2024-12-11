07b540c3ba19dba62bd401e9c5ba6bc44830af19
George Tetenkov <george.tetenkov@softswiss.com>
2024-12-11 22:47:35 +0100

    calculator tests

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,3 +1,5 @@
+package calculator;
+
 public class Calculator {
 
     public int add(int x, int y) {
---
+++ src/test/java/junit/calculator/CalculatorTests.java
@@ -0,0 +1,46 @@
+package junit.calculator;
+
+import calculator.Calculator;
+import org.junit.Test;
+
+import static org.junit.Assert.assertThrows;
+import static org.testng.AssertJUnit.assertEquals;
+
+
+public class CalculatorTests {
+
+    Calculator calculator = new Calculator();
+    @Test
+    public void TestsSum(){
+        assertEquals("Сложение посчитано не верно",5, calculator.add(2, 3));
+        assertEquals("Сложение посчитано не верно",-1, calculator.add(2, -3));
+        assertEquals("Сложение посчитано не верно",1, calculator.add(-2, 3));
+        assertEquals("Сложение посчитано не верно",-5, calculator.add(-2, -3));
+    }
+
+    @Test
+    public void TestSubstract(){
+        assertEquals("Разница посчитана не верно",5, calculator.subtract(2, -3));
+        assertEquals("Разница посчитана не верно",-1, calculator.subtract(2, 3));
+        assertEquals("Разница посчитана не верно",-5, calculator.subtract(-2, 3));
+        assertEquals("Разница посчитана не верно",1, calculator.subtract(-2, -3));
+    }
+    @Test
+    public void TestMultiply(){
+        assertEquals("Сложение посчитанао не верно",0, calculator.multiply(3, 0));
+        assertEquals("Сложение посчитанао не верно",0, calculator.multiply(0, 3));
+        assertEquals("Сложение посчитанао не верно",-3, calculator.multiply(3, -1));
+        assertEquals("Сложение посчитанао не верно",-3, calculator.multiply(-1, 3));
+        assertEquals("Сложение посчитанао не верно",12, calculator.multiply(4, 3));
+    }
+    @Test (expected = ArithmeticException.class)
+    public void TestDivision(){
+        assertEquals("Деление посчитанао не верно",1, calculator.divide(3, 3));
+        assertEquals("Деление посчитанао не верно",3, calculator.divide(3, 1));
+        assertEquals("Деление посчитанао не верно",0, calculator.divide(1, 3));
+        assertEquals("Деление посчитанао не верно",-5, calculator.divide(15, -3));
+        assertEquals("Деление посчитанао не верно",-5, calculator.divide(-15, 3));
+        assertEquals("Деление посчитанао не верно",0, calculator.divide(0, 3));
+        assertEquals("Деление посчитанао не верно",0, calculator.divide(3, 0));
+    }
+}
