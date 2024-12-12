6e3523b12242de9d16cfaeb822da530ec6dacb03
George Tetenkov <george.tetenkov@softswiss.com>
2024-12-12 14:11:34 +0100

    EvenOddCheckerTests

--- src/test/java/junit/evenoddchecker/EvenOddCheckerTests.java
+++ src/test/java/junit/evenoddchecker/EvenOddCheckerTests.java
@@ -1,4 +1,20 @@
 package junit.evenoddchecker;
 
+import evenoddchecker.EvenOddChecker;
+import org.junit.Assert;
+import org.junit.Test;
+
+
 public class EvenOddCheckerTests {
+    @Test
+    public void testEvenNumber(){
+        Assert.assertEquals("2 должно быть четным", true, EvenOddChecker.check(2));
+        Assert.assertEquals("-2 должно быть четным", true, EvenOddChecker.check(-2));
+        Assert.assertEquals("0 должно быть четным", true, EvenOddChecker.check(0));
+    }
+    @Test
+    public void testOddNumber(){
+        Assert.assertEquals("3 должно быть нечетным", false, EvenOddChecker.check(3));
+        Assert.assertEquals("-3 должно быть нечетным", false, EvenOddChecker.check(-3));
+    }
 }
